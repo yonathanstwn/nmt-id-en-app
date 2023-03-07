b8bc3234edf79fd83ec6986fe9cf1f9df72bfc37
Yonathan Setiawan <yonathanbali@gmail.com>
2023-03-07 01:56:59 +0000

    prepare scripts for HPC

--- Transformers/HPCTrainScripts/train-opus-mt-en-id-ccmatrix-lr-4.sh
+++ Transformers/HPCTrainScripts/train-opus-mt-en-id-ccmatrix-lr-4.sh
@@ -1,4 +1,9 @@
+#!/bin/bash -l
+#SBATCH --output=/scratch/users/%u/train-opus-mt-en-id-ccmatrix-lr-4.out
+#SBATCH --job-name=fypTrain
+#SBATCH --time=0-48:00
 cd /scratch/users/k2036348/nmt-id-en-app/
+conda init
 conda activate venv
 cd Transformers/
 python main1.py
--- Transformers/HPCTrainScripts/train-opus-mt-en-id-ccmatrix-lr-5.sh
+++ Transformers/HPCTrainScripts/train-opus-mt-en-id-ccmatrix-lr-5.sh
@@ -1,4 +1,10 @@
+#!/bin/bash -l
+#SBATCH --output=/scratch/users/%u/train-opus-mt-en-id-ccmatrix-lr-5.out
+#SBATCH --job-name=fypTrain
+#SBATCH --time=0-48:00
+#SBATCH --gres=gpu
 cd /scratch/users/k2036348/nmt-id-en-app/
+conda init
 conda activate venv
 cd Transformers/
 python main3.py
