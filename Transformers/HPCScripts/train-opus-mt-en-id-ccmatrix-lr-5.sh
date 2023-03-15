4dc0ae86c4cf5c716e819d291e25dea99e849099
Yonathan Setiawan <yonathanbali@gmail.com>
2023-03-15 22:38:49 +0000

    fix HPC script

--- Transformers/HPCScripts/train-opus-mt-en-id-ccmatrix-lr-5.sh
+++ Transformers/HPCScripts/train-opus-mt-en-id-ccmatrix-lr-5.sh
@@ -2,12 +2,12 @@
 #SBATCH --output=/scratch/users/%u/JobOut/train-opus-mt-en-id-ccmatrix-lr-5/%j.out
 #SBATCH --job-name=fypTrain
 #SBATCH --time=0-48:00
-git lfs install
 export TRANSFORMERS_CACHE=/scratch/users/k2036348/.cache
 export HF_HOME=/scratch/users/k2036348/.cache
 module load anaconda3/2021.05-gcc-9.4.0
 source /users/${USER}/.bashrc
 source activate /scratch/users/k2036348/nmt-id-en-app/venv
+git lfs install
 cd /scratch/users/k2036348/nmt-id-en-app/
 cd Transformers/
 python main.py TRAIN_OPUS_EN_ID_CCMATRIX_LR_5
--- Transformers/HPCScripts/train-opus-mt-id-en-ccmatrix.sh
+++ Transformers/HPCScripts/train-opus-mt-id-en-ccmatrix.sh
@@ -2,12 +2,12 @@
 #SBATCH --output=/scratch/users/%u/JobOut/train-opus-mt-id-en-ccmatrix/%j.out
 #SBATCH --job-name=fypTrain
 #SBATCH --time=0-48:00
-git lfs install
 export TRANSFORMERS_CACHE=/scratch/users/k2036348/.cache
 export HF_HOME=/scratch/users/k2036348/.cache
 module load anaconda3/2021.05-gcc-9.4.0
 source /users/${USER}/.bashrc
 source activate /scratch/users/k2036348/nmt-id-en-app/venv
+git lfs install
 cd /scratch/users/k2036348/nmt-id-en-app/
 cd Transformers/
 python main.py TRAIN_OPUS_ID_EN_CCMATRIX
