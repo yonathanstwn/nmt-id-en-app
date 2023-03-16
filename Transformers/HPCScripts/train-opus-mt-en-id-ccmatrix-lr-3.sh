8b886be8cd4244591da66066ff64e851cfad5e44
Yonathan Setiawan <yonathanbali@gmail.com>
2023-03-16 12:33:08 +0000

    fix scripts

--- Transformers/HPCScripts/train-opus-mt-en-id-ccmatrix-lr-3.sh
+++ Transformers/HPCScripts/train-opus-mt-en-id-ccmatrix-lr-3.sh
@@ -7,7 +7,7 @@ export TRANSFORMERS_CACHE=/scratch/users/k2036348/.cache
 export HF_HOME=/scratch/users/k2036348/.cache
 module load anaconda3/2021.05-gcc-9.4.0
 source /users/${USER}/.bashrc
-source activate /scratch/users/k2036348/nmt-id-en-app/venv
+source activate /scratch/users/k2036348/nmt-id-en-app/Transformers/HPCScripts/venv
 git lfs install
 cd /scratch/users/k2036348/nmt-id-en-app/
 cd Transformers/
