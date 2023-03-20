f4cf20113e332a56eae6cebe470d2a8157b51188
Yonathan Setiawan <yonathanbali@gmail.com>
2023-03-20 15:04:23 +0000

    add openai to requirements

--- Scraper/main.py
+++ Scraper/main.py
@@ -2,10 +2,19 @@
 Main runner file to scrape or generate English - Colloquial Indonesian dataset
 from OpenAI GPT-3.5 API endpoints.
 """
-
+import openai
 
 def main():
-    pass
+    openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    )
+
 
 if __name__ == '__main__':
     main()
