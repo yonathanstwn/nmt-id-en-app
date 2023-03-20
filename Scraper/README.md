5ba9018dea55e0670c65aa3c4bc1606cba7729bc
Yonathan Setiawan <yonathanbali@gmail.com>
2023-03-20 17:53:36 +0000

    add sql alchemy to requirements

---
+++ Scraper/README.md
@@ -0,0 +1,16 @@
+# Overview
+
+...
+
+
+# Setup
+
+Please ensure you have installed all the requirements from the root folder which includes the `openai` python library.
+
+Create a json file in the `Scraper` folder called `api_key.json` with the following schema:
+
+```json
+{
+    "key": "key123"
+}
+```
--- Scraper/main.py
+++ Scraper/main.py
@@ -3,17 +3,33 @@ Main runner file to scrape or generate English - Colloquial Indonesian dataset
 from OpenAI GPT-3.5 API endpoints.
 """
 import openai
+import json
+from datasets import load_dataset
+
 
 def main():
-    openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}
+    """Main program to be run when running this file"""
+
+    # Load OpenSubtitles dataset
+    dataset = load_dataset("open_subtitles", lang1="en", lang2="id")[
+        'train'].select(range(50_000))
+
+    # Setup OpenAI API key
+    with open('api_key.json', 'r') as f:
+        openai.api_key = json.load(f)['key']
+
+    WORD_LIMIT = 20
+
+    # Send prompt as an API request
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "English to colloquial Indonesian translator."},
+            {"role": "user", "content": "I want to go to the beach with my friends after I study."}
         ]
     )
+
+    print(response)
 
 
 if __name__ == '__main__':
