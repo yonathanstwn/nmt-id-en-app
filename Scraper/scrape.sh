d76c9a65119fb792b51b649925b94b2c8af80fa7
Yonathan Setiawan <k2036348@erc-hpc-login2.create.kcl.ac.uk>
2023-03-22 15:32:19 +0000

    update scraper script

--- Scraper/scrape.sh
+++ Scraper/scrape.sh
@@ -3,6 +3,7 @@
 #SBATCH --job-name=scrape
 #SBATCH --time=0-24:00
 #SBATCH --mem=4000
+export HF_HOME=/scratch/users/k2036348/.cache
 module load anaconda3/2021.05-gcc-9.4.0
 source /users/${USER}/.bashrc
 source activate /scratch/users/k2036348/nmt-id-en-app/Scraper/venv
